41f9eb2898fff5d34c6f07e63cc154eb0101b008
MaxBubblegum47 <lorenzostigliano@gmail.com>
2022-02-25 12:22:39 +0100

    :adhesive_bandage: Trying to not explode

--- llvm/include/llvm/Object/ELFObjectFile.h
+++ llvm/include/llvm/Object/ELFObjectFile.h
@@ -1279,13 +1279,8 @@ template <class ELFT> Triple::ArchType ELFObjectFile<ELFT>::getArch() const {
   case ELF::EM_S390:
     return Triple::systemz;
 
-    case ELF::EM_MBLAZE:
-    switch (EF.getHeader()->e_ident[ELF::EI_CLASS]) {
-    case ELF::ELFCLASS32:
+  case ELF::EM_MBLAZE:
     return Triple::mblaze;
-    default:
-      report_fatal_error("Invalid ELFCLASS!");
-    }
 
 
   case ELF::EM_SPARC:
