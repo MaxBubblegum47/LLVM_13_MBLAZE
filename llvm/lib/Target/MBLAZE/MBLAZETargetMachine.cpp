218b3fbb5dd0d601ef12d81d858057887529e7ea
maxbubblegum47 <lorenzostigliano@gmail.com>
2022-05-20 09:40:37 +0200

    :memo: Define Target Machine

--- llvm/lib/Target/MBLAZE/MBLAZETargetMachine.cpp
+++ llvm/lib/Target/MBLAZE/MBLAZETargetMachine.cpp
@@ -11,6 +11,8 @@
 //===----------------------------------------------------------------------===//
 //#include "MBLAZETargetMachine.h"
 #include "MBLAZE.h"
+#include "TargetInfo/MBLAZETargetInfo.h"
+#include "MBLAZETargetMachine.h"
 #include "llvm/IR/Attributes.h"
 #include "llvm/IR/Function.h"
 #include "llvm/Support/CodeGen.h"
@@ -20,6 +22,35 @@
 #include  "llvm/Target/TargetOptions.h"
 
 using namespace llvm;
-#define DEBUG_TYPE "mblaze"
-extern "C" void LLVMInitializeMBLAZETarget() {
+
+extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeMBLAZETarget() {
+  RegisterTargetMachine<MBLAZETargetMachine> X(getTheMBLAZETarget());
+//   auto *PR = PassRegistry::getPassRegistry();
+//   initializeGlobalISel(*PR);
+//   initializeMBLAZEMergeBaseOffsetOptPass(*PR);
+//   initializeMBLAZEExpandPseudoPass(*PR);
+//   initializeMBLAZEInsertVSETVLIPass(*PR);
+}
+
+namespace {
+    class MBLAZEPassConfig : public TargetPassConfig {
+    public:
+        MBLAZEPassConfig(MBLAZETargetMachine &TM, PassManagerBase
+        &PM)
+        : TargetPassConfig(TM, PM) {}
+ 
+        MBLAZETargetMachine &getMBLAZETargetMachine() const {
+            return getTM<MBLAZETargetMachine>();
+        }
+
+        // bool addInstSelector() override {
+        //     addPass(createMBLAZEISelDag(getMBLAZETargetMachine(),
+        //     getOptLevel()));
+        //     return false;
+        // }
+    };
+} // namespace
+
+TargetPassConfig *MBLAZETargetMachine::createPassConfig(PassManagerBase &PM) {
+    return new MBLAZEPassConfig(*this, PM);
 }
