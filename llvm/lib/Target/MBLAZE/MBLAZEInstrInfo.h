8867ce7bea19a372a958a4a48a1fc6b1ae720d0c
maxbubblegum47 <lorenzostigliano@gmail.com>
2022-07-31 21:39:34 +0200

    Copy to physical register

--- llvm/lib/Target/MBLAZE/MBLAZEInstrInfo.h
+++ llvm/lib/Target/MBLAZE/MBLAZEInstrInfo.h
@@ -15,8 +15,13 @@ class MBLAZESubtarget;
 class MBLAZEInstrInfo : public MBLAZEGenInstrInfo {
 
 public:
-  explicit MBLAZEInstrInfo(MBLAZESubtarget &STI);
-    };
-}
+  explicit MBLAZEInstrInfo(MBLAZESubtarget &STI); // STI refers to subtarget feature that right now i don't have
+    
+  void copyPhysReg(MachineBasicBlock &MBB, MachineBasicBlock::iterator MBBI,
+                  const DebugLoc &DL, MCRegister DstReg, MCRegister SrcReg,
+                  bool KillSrc) const override;  
+      
+    };                                            // but at the same time i'm not super sure about how to do a function
+}                                                 // without inlcuding it
 
 #endif
