25fe821e2b4215200eec72d24ba96e4db4c4f857
MaxBubblegum47 <lorenzostigliano@gmail.com>
2022-08-13 11:04:55 +0200

    Removing pseudotables

--- llvm/lib/Target/MBLAZE/MBLAZEInstrInfo.cpp
+++ llvm/lib/Target/MBLAZE/MBLAZEInstrInfo.cpp
@@ -17,14 +17,6 @@ using namespace llvm;
 #define GET_INSTRINFO_CTOR_DTOR
 #include "MBLAZEGenInstrInfo.inc"
 
-namespace llvm {
-namespace MBLAZEVPseudosTable {
-
-using namespace MBLAZE;
-
-} // namespace MBLAZEVPseudosTable
-} // namespace llvm
-
 MBLAZEInstrInfo::MBLAZEInstrInfo(MBLAZESubtarget &STI)
     : MBLAZEGenInstrInfo(MBLAZE::ADJCALLSTACKDOWN, MBLAZE::ADJCALLSTACKUP)
     {}
