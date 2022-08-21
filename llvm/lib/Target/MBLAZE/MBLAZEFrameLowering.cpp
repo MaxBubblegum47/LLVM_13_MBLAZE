f69bc7e0471c76ee5e1ac8fa9e858a9d0b4ae16e
MaxBubblegum47 <lorenzostigliano@gmail.com>
2022-08-21 14:56:01 +0200

    First commit frame lowering

---
+++ llvm/lib/Target/MBLAZE/MBLAZEFrameLowering.cpp
@@ -0,0 +1,14 @@
+#include "MBLAZEFrameLowering.h"
+#include "MBLAZEInstrInfo.h"
+#include "llvm/CodeGen/MachineFrameInfo.h"
+#include "llvm/CodeGen/MachineFunction.h"
+#include "llvm/CodeGen/MachineInstrBuilder.h"
+#include "llvm/CodeGen/MachineModuleInfo.h"
+#include "llvm/CodeGen/MachineRegisterInfo.h"
+#include "llvm/IR/DataLayout.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Target/TargetOptions.h"
+
+using namespace llvm;
+
+// I need to create an epilogue and prologue function
