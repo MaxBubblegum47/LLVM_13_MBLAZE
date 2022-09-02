ead134787faa2f8fbbaa31da27d04e62c6c35338
maxbubblegum47 <lorenzostigliano@gmail.com>
2022-09-02 11:28:56 +0200

    Changing some include; testing RISCV configuration

--- llvm/lib/Target/MBLAZE/MBLAZEFrameLowering.cpp
+++ llvm/lib/Target/MBLAZE/MBLAZEFrameLowering.cpp
@@ -3,11 +3,10 @@
 #include "llvm/CodeGen/MachineFrameInfo.h"
 #include "llvm/CodeGen/MachineFunction.h"
 #include "llvm/CodeGen/MachineInstrBuilder.h"
-#include "llvm/CodeGen/MachineModuleInfo.h"
 #include "llvm/CodeGen/MachineRegisterInfo.h"
-#include "llvm/IR/DataLayout.h"
-#include "llvm/IR/Function.h"
-#include "llvm/Target/TargetOptions.h"
+#include "llvm/CodeGen/RegisterScavenging.h"
+#include "llvm/IR/DiagnosticInfo.h"
+#include "llvm/MC/MCDwarf.h"
 
 using namespace llvm;
 
