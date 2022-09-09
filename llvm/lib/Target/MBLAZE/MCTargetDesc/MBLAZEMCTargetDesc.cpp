2de0df06f905c6b8098cbae11829aead357b867d
MaxBubbelgum47 <lorenzostigliano@gmail.com>
2022-09-09 21:54:04 +0200

    First commit, test

--- llvm/lib/Target/MBLAZE/MCTargetDesc/MBLAZEMCTargetDesc.cpp
+++ llvm/lib/Target/MBLAZE/MCTargetDesc/MBLAZEMCTargetDesc.cpp
@@ -1,60 +1,83 @@
-//===-- MBLAZEMCTargetDesc.cpp - MBLAZE Target Descriptions -------------------===//
+//===- MBLAZEMCTargetDesc.cpp - MBLAZE Target Descriptions ------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-///
-/// This file provides MBLAZE specific target descriptions.
-///
+//
+// This file provides MBLAZE specific target descriptions.
+//
 //===----------------------------------------------------------------------===//
-#include <iostream>
-#include "MBLAZEMCTargetDesc.h"
 
+#include "MBLAZEMCTargetDesc.h"
+//#include "MBLAZEInstPrinter.h"
+#include "MBLAZEMCAsmInfo.h"
+//#include "MBLAZETargetStreamer.h"
 #include "TargetInfo/MBLAZETargetInfo.h"
+#include "llvm/MC/MCDwarf.h"
 #include "llvm/MC/MCInstrInfo.h"
 #include "llvm/MC/MCRegisterInfo.h"
 #include "llvm/MC/MCSubtargetInfo.h"
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/FormattedStream.h"
 #include "llvm/Support/TargetRegistry.h"
-#include "llvm/MC/MCStreamer.h"
+
+using namespace llvm;
+
 #define GET_INSTRINFO_MC_DESC
 #include "MBLAZEGenInstrInfo.inc"
+
+#define GET_SUBTARGETINFO_MC_DESC
+#include "MBLAZEGenSubtargetInfo.inc"
 
 #define GET_REGINFO_MC_DESC
 #include "MBLAZEGenRegisterInfo.inc"
 
-using namespace llvm;
-
-
-
 static MCInstrInfo *createMBLAZEMCInstrInfo() {
-  MCInstrInfo *Info = new MCInstrInfo();
-  InitMBLAZEMCInstrInfo(Info);
-  return Info;
+  auto *X = new MCInstrInfo();
+  InitMBLAZEMCInstrInfo(X);
+  return X;
 }
 
 static MCRegisterInfo *createMBLAZEMCRegisterInfo(const Triple &TT) {
-  MCRegisterInfo *Info = new MCRegisterInfo();
-  InitMBLAZEMCRegisterInfo(Info, MBLAZE::R1);
-  return Info;
+  auto *X = new MCRegisterInfo();
+  InitMBLAZEMCRegisterInfo(X, MBLAZE::SW);
+  return X;
 }
-static MCSubtargetInfo* createMBLAZEMCSubtargetInfo 	( 	const Triple &  	TT,
-		StringRef  	CPU,
-		StringRef  	FS 
-	) 		{
-		
-		
+
+static MCSubtargetInfo *createMBLAZEMCSubtargetInfo(const Triple &TT,
+                                                 StringRef CPU, StringRef FS) {
+  return createMBLAZEMCSubtargetInfoImpl(TT, CPU, /*TuneCPU=*/CPU, FS);
+}
+
+static MCAsmInfo *createMBLAZEMCAsmInfo(const MCRegisterInfo &MRI,
+                                     const Triple &TT,
+                                     const MCTargetOptions &Options) {
+  MCAsmInfo *MAI = new MBLAZEMCAsmInfo(TT);
+
+  // Initial state of the frame pointer is SP.
+  MCCFIInstruction Inst = MCCFIInstruction::cfiDefCfa(nullptr, MBLAZE::SW, 0);
+  MAI->addInitialFrameState(Inst);
+
+  return MAI;
 }
 
 
+// Force static initialization.
 extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeMBLAZETargetMC() {
-  auto &MBLAZETarget = getTheMBLAZETarget();
-  // TargetRegistry::RegisterMCAsmBackend(MBLAZETarget, createMBLAZEAsmBackend);
-  // TargetRegistry::RegisterMCAsmInfo(MBLAZETarget, createMBLAZEMCAsmInfo);
-  TargetRegistry::RegisterMCInstrInfo(MBLAZETarget, createMBLAZEMCInstrInfo);
-  TargetRegistry::RegisterMCRegInfo(MBLAZETarget, createMBLAZEMCRegisterInfo);
-  TargetRegistry::RegisterMCSubtargetInfo(MBLAZETarget, createMBLAZEMCSubtargetInfo);
-  // TargetRegistry::RegisterMCCodeEmitter(MBLAZETarget, createMBLAZEMCCodeEmitter);
+  // Register the MC asm info.
+  Target &TheMBLAZETarget = getTheMBLAZETarget();
+  RegisterMCAsmInfoFn X(TheMBLAZETarget, createMBLAZEMCAsmInfo);
+
+  // Register the MC instruction info.
+  TargetRegistry::RegisterMCInstrInfo(TheMBLAZETarget, createMBLAZEMCInstrInfo);
+
+  // Register the MC register info.
+  TargetRegistry::RegisterMCRegInfo(TheMBLAZETarget, createMBLAZEMCRegisterInfo);
+
+  // Register the MC subtarget info.
+  TargetRegistry::RegisterMCSubtargetInfo(TheMBLAZETarget,
+                                          createMBLAZEMCSubtargetInfo);
 
 }
