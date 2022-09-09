e4ff406d84418d365279fef9135a7ae37d4309e8
MaxBubbelgum47 <lorenzostigliano@gmail.com>
2022-09-09 21:51:45 +0200

    Testing

--- llvm/lib/Target/MBLAZE/MBLAZERegisterInfo.cpp
+++ llvm/lib/Target/MBLAZE/MBLAZERegisterInfo.cpp
@@ -2,17 +2,22 @@
 #include "MBLAZE.h"
 // #include "MBLAZEMachineFunctionInfo.h" --> From MBLAZE Example we should have also this at a certain point
 // #include "MBLAZESubtarget.h"
+#include "llvm/ADT/BitVector.h"
 #include "llvm/CodeGen/MachineFrameInfo.h"
 #include "llvm/CodeGen/MachineFunction.h"
 #include "llvm/CodeGen/MachineInstrBuilder.h"
+#include "llvm/CodeGen/MachineModuleInfo.h"
+#include "llvm/CodeGen/MachineRegisterInfo.h"
 #include "llvm/CodeGen/RegisterScavenging.h"
 #include "llvm/CodeGen/TargetFrameLowering.h"
-#include "llvm/CodeGen/TargetInstrInfo.h"
-#include "llvm/IR/DebugInfoMetadata.h"
-#include "llvm/Support/ErrorHandling.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Target/TargetMachine.h"
+#include "llvm/Target/TargetOptions.h"
 
-// BUG --> error: invalid ‘static_cast’ from type ‘const llvm::TargetFrameLowering*’ to type ‘const llvm::MBLAZEFrameLowering*’
-// #define GET_REGINFO_TARGET_DESC
-// #include "MBLAZEGenRegisterInfo.inc" 
 
 using namespace llvm;
+
+// BUG --> error: invalid ‘static_cast’ from type ‘const llvm::TargetFrameLowering*’ to type ‘const llvm::MBLAZEFrameLowering*’
+#define GET_REGINFO_TARGET_DESC
+#include "MBLAZEGenRegisterInfo.inc" 
