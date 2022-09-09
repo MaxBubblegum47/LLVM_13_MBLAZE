8ee05345d89b8cd547820089a8eec70497c37707
MaxBubbelgum47 <lorenzostigliano@gmail.com>
2022-09-09 21:52:09 +0200

    Testing

--- llvm/lib/Target/MBLAZE/MBLAZERegisterInfo.h
+++ llvm/lib/Target/MBLAZE/MBLAZERegisterInfo.h
@@ -1,13 +1,21 @@
 #ifndef MBLAZEREGISTERINFO_H
 #define MBLAZEREGISTERINFO_H
 
-#include "MBLAZE.h"
 #include "llvm/CodeGen/TargetRegisterInfo.h"
 
 // BUG: 
-// #define GET_REGINFO_HEADER
-// #include "MBLAZEGenRegisterInfo.inc" 
+#define GET_REGINFO_HEADER
+#include "MBLAZEGenRegisterInfo.inc" 
 
+namespace llvm{
+    class TargetInstrInfo;
+
+    struct MBLAZERegisterInfo : public MBLAZEGenRegisterInfo{
+        public:
+            MBLAZERegisterInfo();
+    };
+
+}
 
 
 #endif
