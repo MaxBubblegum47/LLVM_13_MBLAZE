f5ca39cb12bc087a9a653ccfa423a762f6b75ddc
MaxBubblegum47 <lorenzostigliano@gmail.com>
2022-09-14 20:58:43 +0200

    First Commit

---
+++ llvm/lib/Target/MBLAZE/MBLAZEISelDAGToDAG.cpp
@@ -0,0 +1,39 @@
+#include "MBLAZE.h"
+#include "MBLAZETargetMachine.h"
+#include "llvm/CodeGen/MachineFrameInfo.h"
+#include "llvm/CodeGen/MachineFunction.h"
+#include "llvm/CodeGen/MachineInstrBuilder.h"
+#include "llvm/CodeGen/MachineRegisterInfo.h"
+#include "llvm/CodeGen/SelectionDAG.h"
+#include "llvm/CodeGen/SelectionDAGISel.h"
+#include "llvm/CodeGen/TargetLowering.h"
+#include "llvm/IR/CallingConv.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Intrinsics.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/Support/Compiler.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+namespace {
+    class MBLAZEDAGToDAGISel : public SelectionDAGISel {
+    public:
+        MBLAZEDAGToDAGISel(MBLAZETargetMachine &TM, CodeGenOpt::Level OptLevel)
+            : SelectionDAGISel(TM, OptLevel) {}
+
+    #include "MBLAZEGenDAGISel.inc"
+
+
+    };
+}
+
+
+FunctionPass *llvm::createMBLAZEISelDag(MBLAZETargetMachine &TM,
+                                     CodeGenOpt::Level OptLevel) {
+  return new MBLAZEDAGToDAGISel(TM, OptLevel);
+}
