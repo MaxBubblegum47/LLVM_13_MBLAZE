7817fd2b2497aeb0daf51d782fa15dddb234740f
MaxBubblegum47 <lorenzostigliano@gmail.com>
2022-09-30 01:08:15 +0200

    Trying to complete registerinfo

--- llvm/lib/Target/MBLAZE/MBLAZERegisterInfo.h
+++ llvm/lib/Target/MBLAZE/MBLAZERegisterInfo.h
@@ -4,9 +4,17 @@
 #include "llvm/CodeGen/TargetRegisterInfo.h"
 
 // BUG: 
-// #define GET_REGINFO_HEADER
-// #include "MBLAZEGenRegisterInfo.inc" 
+#define GET_REGINFO_HEADER
+#include "MBLAZEGenRegisterInfo.inc" 
 
+namespace llvm {
 
+class TargetInstrInfo;
+
+struct MBLAZERegisterInfo : public MBLAZEGenRegisterInfo {
+public:
+  MBLAZERegisterInfo();
+
+}
 
 #endif
