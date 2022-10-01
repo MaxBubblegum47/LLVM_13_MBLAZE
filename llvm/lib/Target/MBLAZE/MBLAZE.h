5950d1646c1648a14d41f7c2faf0d832c5a09b6f
MaxBubblegum47 <lorenzostigliano@gmail.com>
2022-10-02 00:34:21 +0200

    Cleaning

--- llvm/lib/Target/MBLAZE/MBLAZE.h
+++ llvm/lib/Target/MBLAZE/MBLAZE.h
@@ -11,90 +11,24 @@
 // the LLVM MBLAZE back-end.
 //
 //===----------------------------------------------------------------------===//
-
-
 #ifndef LLVM_LIB_TARGET_MBLAZE_MBLAZE_H
 #define LLVM_LIB_TARGET_MBLAZE_MBLAZE_H
 
+//#include "MBLAZEConfig.h"
 #include "MCTargetDesc/MBLAZEMCTargetDesc.h"
 #include "llvm/Target/TargetMachine.h"
 
 namespace llvm {
+    
+    class FunctionPass;
+    class MBLAZETargetMachine;
 
-class FunctionPass;
-class MBLAZETargetMachine;
-
-FunctionPass *createMBLAZEISelDag(MBLAZETargetMachine &TM,
+    FunctionPass *createMBLAZEISelDag(MBLAZETargetMachine &TM,
                                CodeGenOpt::Level OptLevel);
-FunctionPass *createMBLAZEExpandPseudosPass();
-FunctionPass *createMBLAZEOptAddrMode();
-FunctionPass *createMBLAZEBranchFinalizePass();
 
-} // end namespace llvm
+    FunctionPass *createMBLAZEExpandPseudosPass();
+    FunctionPass *createMBLAZEOptAddrMode();
+    FunctionPass *createMBLAZEBranchFinalizePass();
 
-#endif // LLVM_LIB_TARGET_MBLAZE_MBLAZE_H
-
-
-
-
-
-//=================================================================================
-//  OLD STUFF
-//=================================================================================
-
-// #ifndef LLVM_LIB_TARGET_MBLAZE_MBLAZE_H
-// #define LLVM_LIB_TARGET_MBLAZE_MBLAZE_H
-
-// //#include "MBLAZEConfig.h"
-// #include "MCTargetDesc/MBLAZEMCTargetDesc.h"
-// #include "llvm/Target/TargetMachine.h"
-
-// namespace llvm {
-//     class MBLAZERegisterInfo;
-//     class MBLAZESubtarget;
-//     class MBLAZETargetMachine;
-//     class AsmPrinter;
-//     class FunctionPass;
-//     class InstructionSelector;
-//     class MCInst;
-//     class MCOperand;
-//     class MachineInstr;
-//     class MachineOperand;
-//     class PassRegistry;
-
-//     bool lowerMBLAZEMachineInstrToMCInst(const MachineInstr *MI, MCInst &OutMI,
-//                                     AsmPrinter &AP);
-    
-//     bool LowerMBLAZEMachineOperandToMCOperand(const MachineOperand &MO,
-//                                          MCOperand &MCOp, const AsmPrinter &AP);
-
-
-//     FunctionPass *createMBLAZEISelDag(MBLAZETargetMachine &TM,
-//                                CodeGenOpt::Level OptLevel);
-// FunctionPass *createMBLAZEExpandPseudosPass();
-// FunctionPass *createMBLAZEOptAddrMode();
-// FunctionPass *createMBLAZEBranchFinalizePass();
-
-    // FunctionPass *createMBLAZEISelDag(MBLAZETargetMachine &TM);
-
-    // FunctionPass *createMBLAZEMergeBaseOffsetOptPass();
-    // void initializeMBLAZEMergeBaseOffsetOptPass(PassRegistry &);
-
-    // FunctionPass *createMBLAZEExpandPseudoPass();
-    // void initializeMBLAZEExpandPseudoPass(PassRegistry &);
-
-    // FunctionPass *createMBLAZEExpandAtomicPseudoPass();
-    // void initializeMBLAZEExpandAtomicPseudoPass(PassRegistry &);
-
-    // FunctionPass *createMBLAZEInsertVSETVLIPass();
-    // void initializeMBLAZEInsertVSETVLIPass(PassRegistry &);
-
-    // InstructionSelector *createMBLAZEInstructionSelector(const MBLAZETargetMachine &,
-    //                                                     MBLAZESubtarget &,
-    //                                                     MBLAZERegisterBankInfo &);
-    
-
-    // // idk what is this, maybe some legacy code
-    // FunctionPass *createMBLAZEDelaySlotFillerPass(MBLAZETargetMachine &TM);
-// } // end namespace llvm;
-// #endif
+} // end namespace llvm;
+#endif
