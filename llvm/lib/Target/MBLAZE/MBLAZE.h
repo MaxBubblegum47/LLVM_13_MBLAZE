c77b4f2a673c9b1f5638701b6027e72d3c922995
MaxBubblegum47 <lorenzostigliano@gmail.com>
2022-10-01 20:10:28 +0200

    Fixing some issues, taking example from ARC

--- llvm/lib/Target/MBLAZE/MBLAZE.h
+++ llvm/lib/Target/MBLAZE/MBLAZE.h
@@ -11,38 +11,69 @@
 // the LLVM MBLAZE back-end.
 //
 //===----------------------------------------------------------------------===//
+
+
 #ifndef LLVM_LIB_TARGET_MBLAZE_MBLAZE_H
 #define LLVM_LIB_TARGET_MBLAZE_MBLAZE_H
 
-//#include "MBLAZEConfig.h"
 #include "MCTargetDesc/MBLAZEMCTargetDesc.h"
 #include "llvm/Target/TargetMachine.h"
 
 namespace llvm {
-    class MBLAZERegisterInfo;
-    class MBLAZESubtarget;
-    class MBLAZETargetMachine;
-    class AsmPrinter;
-    class FunctionPass;
-    class InstructionSelector;
-    class MCInst;
-    class MCOperand;
-    class MachineInstr;
-    class MachineOperand;
-    class PassRegistry;
 
-    bool lowerMBLAZEMachineInstrToMCInst(const MachineInstr *MI, MCInst &OutMI,
-                                    AsmPrinter &AP);
-    
-    bool LowerMBLAZEMachineOperandToMCOperand(const MachineOperand &MO,
-                                         MCOperand &MCOp, const AsmPrinter &AP);
+class FunctionPass;
+class MBLAZETargetMachine;
 
-
-    FunctionPass *createMBLAZEISelDag(MBLAZETargetMachine &TM,
+FunctionPass *createMBLAZEISelDag(MBLAZETargetMachine &TM,
                                CodeGenOpt::Level OptLevel);
 FunctionPass *createMBLAZEExpandPseudosPass();
 FunctionPass *createMBLAZEOptAddrMode();
 FunctionPass *createMBLAZEBranchFinalizePass();
+
+} // end namespace llvm
+
+#endif // LLVM_LIB_TARGET_MBLAZE_MBLAZE_H
+
+
+
+
+
+//=================================================================================
+//  OLD STUFF
+//=================================================================================
+
+// #ifndef LLVM_LIB_TARGET_MBLAZE_MBLAZE_H
+// #define LLVM_LIB_TARGET_MBLAZE_MBLAZE_H
+
+// //#include "MBLAZEConfig.h"
+// #include "MCTargetDesc/MBLAZEMCTargetDesc.h"
+// #include "llvm/Target/TargetMachine.h"
+
+// namespace llvm {
+//     class MBLAZERegisterInfo;
+//     class MBLAZESubtarget;
+//     class MBLAZETargetMachine;
+//     class AsmPrinter;
+//     class FunctionPass;
+//     class InstructionSelector;
+//     class MCInst;
+//     class MCOperand;
+//     class MachineInstr;
+//     class MachineOperand;
+//     class PassRegistry;
+
+//     bool lowerMBLAZEMachineInstrToMCInst(const MachineInstr *MI, MCInst &OutMI,
+//                                     AsmPrinter &AP);
+    
+//     bool LowerMBLAZEMachineOperandToMCOperand(const MachineOperand &MO,
+//                                          MCOperand &MCOp, const AsmPrinter &AP);
+
+
+//     FunctionPass *createMBLAZEISelDag(MBLAZETargetMachine &TM,
+//                                CodeGenOpt::Level OptLevel);
+// FunctionPass *createMBLAZEExpandPseudosPass();
+// FunctionPass *createMBLAZEOptAddrMode();
+// FunctionPass *createMBLAZEBranchFinalizePass();
 
     // FunctionPass *createMBLAZEISelDag(MBLAZETargetMachine &TM);
 
@@ -65,5 +96,5 @@ FunctionPass *createMBLAZEBranchFinalizePass();
 
     // // idk what is this, maybe some legacy code
     // FunctionPass *createMBLAZEDelaySlotFillerPass(MBLAZETargetMachine &TM);
-} // end namespace llvm;
-#endif
+// } // end namespace llvm;
+// #endif
