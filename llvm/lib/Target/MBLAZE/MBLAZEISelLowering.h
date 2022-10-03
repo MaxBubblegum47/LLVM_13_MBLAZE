48e084a83bda55c43ea339612ab3c392e1a910de
MaxBubblegum47 <lorenzostigliano@gmail.com>
2022-10-03 18:21:52 +0200

    Uncomment more thing to make everything work

--- llvm/lib/Target/MBLAZE/MBLAZEISelLowering.h
+++ llvm/lib/Target/MBLAZE/MBLAZEISelLowering.h
@@ -25,95 +25,95 @@ class MBLAZESubtarget;
 class MBLAZETargetMachine;
 
 namespace MBLAZEISD {
-    enum NodeType {
-      // Start the numbering from where ISD NodeType finishes.
-      FIRST_NUMBER = ISD::BUILTIN_OP_END,
 
-      // Jump and link (call)
-      JmpLink,
+enum NodeType : unsigned {
+  // Start the numbering where the builtin ops and target ops leave off.
+  FIRST_NUMBER = ISD::BUILTIN_OP_END,
 
-      // Handle gp_rel (small data/bss sections) relocation.
-      GPRel,
+  // Branch and link (call)
+  BL,
 
-      // Select CC Pseudo Instruction
-      Select_CC,
+  // Jump and link (indirect call)
+  JL,
 
-      // Wrap up multiple types of instructions
-      Wrap,
+  // CMP
+  CMP,
 
-      // Integer Compare
-      ICmp,
+  // CMOV
+  CMOV,
 
-      // Return from subroutine
-      Ret,
+  // BRcc
+  BRcc,
 
-      // Return from interrupt
-      IRet
-    };
-  }
+  // Global Address Wrapper
+  GAWRAPPER,
+
+  // return, (j_s [blink])
+  RET
+};
 
 } // end namespace MBLAZEISD
 
-// //===--------------------------------------------------------------------===//
-// // TargetLowering Implementation
-// //===--------------------------------------------------------------------===//
-// class MBLAZETargetLowering : public TargetLowering {
-// public:
-//   explicit MBLAZETargetLowering(const TargetMachine &TM,
-//                              const MBLAZESubtarget &Subtarget);
+//===--------------------------------------------------------------------===//
+// TargetLowering Implementation
+//===--------------------------------------------------------------------===//
+class MBLAZETargetLowering : public TargetLowering {
+public:
+  explicit MBLAZETargetLowering(const TargetMachine &TM,
+                             const MBLAZESubtarget &Subtarget);
 
-//   /// Provide custom lowering hooks for some operations.
-//   SDValue LowerOperation(SDValue Op, SelectionDAG &DAG) const override;
+  /// Provide custom lowering hooks for some operations.
+  SDValue LowerOperation(SDValue Op, SelectionDAG &DAG) const override;
 
-//   /// This method returns the name of a target specific DAG node.
-//   const char *getTargetNodeName(unsigned Opcode) const override;
+  /// This method returns the name of a target specific DAG node.
+  const char *getTargetNodeName(unsigned Opcode) const override;
 
-//   /// Return true if the addressing mode represented by AM is legal for this
-//   /// target, for a load/store of the specified type.
-//   bool isLegalAddressingMode(const DataLayout &DL, const AddrMode &AM, Type *Ty,
-//                              unsigned AS,
-//                              Instruction *I = nullptr) const override;
+  /// Return true if the addressing mode represented by AM is legal for this
+  /// target, for a load/store of the specified type.
+  bool isLegalAddressingMode(const DataLayout &DL, const AddrMode &AM, Type *Ty,
+                             unsigned AS,
+                             Instruction *I = nullptr) const override;
 
-// private:
-//   const MBLAZESubtarget &Subtarget;
+private:
+  const MBLAZESubtarget &Subtarget;
 
-//   // Lower Operand helpers
-//   SDValue LowerCallArguments(SDValue Chain, CallingConv::ID CallConv,
-//                              bool isVarArg,
-//                              const SmallVectorImpl<ISD::InputArg> &Ins,
-//                              SDLoc dl, SelectionDAG &DAG,
-//                              SmallVectorImpl<SDValue> &InVals) const;
-//   // Lower Operand specifics
-//   SDValue LowerJumpTable(SDValue Op, SelectionDAG &DAG) const;
-//   SDValue LowerFRAMEADDR(SDValue Op, SelectionDAG &DAG) const;
-//   SDValue LowerSELECT_CC(SDValue Op, SelectionDAG &DAG) const;
-//   SDValue LowerBR_CC(SDValue Op, SelectionDAG &DAG) const;
-//   SDValue LowerSIGN_EXTEND_INREG(SDValue Op, SelectionDAG &DAG) const;
-//   SDValue LowerGlobalAddress(SDValue Op, SelectionDAG &DAG) const;
-//   SDValue PerformDAGCombine(SDNode *N, DAGCombinerInfo &DCI) const override;
+  // Lower Operand helpers
+  SDValue LowerCallArguments(SDValue Chain, CallingConv::ID CallConv,
+                             bool isVarArg,
+                             const SmallVectorImpl<ISD::InputArg> &Ins,
+                             SDLoc dl, SelectionDAG &DAG,
+                             SmallVectorImpl<SDValue> &InVals) const;
+  // Lower Operand specifics
+  SDValue LowerJumpTable(SDValue Op, SelectionDAG &DAG) const;
+  SDValue LowerFRAMEADDR(SDValue Op, SelectionDAG &DAG) const;
+  SDValue LowerSELECT_CC(SDValue Op, SelectionDAG &DAG) const;
+  SDValue LowerBR_CC(SDValue Op, SelectionDAG &DAG) const;
+  SDValue LowerSIGN_EXTEND_INREG(SDValue Op, SelectionDAG &DAG) const;
+  SDValue LowerGlobalAddress(SDValue Op, SelectionDAG &DAG) const;
+  SDValue PerformDAGCombine(SDNode *N, DAGCombinerInfo &DCI) const override;
 
-//   SDValue LowerFormalArguments(SDValue Chain, CallingConv::ID CallConv,
-//                                bool isVarArg,
-//                                const SmallVectorImpl<ISD::InputArg> &Ins,
-//                                const SDLoc &dl, SelectionDAG &DAG,
-//                                SmallVectorImpl<SDValue> &InVals) const override;
+  SDValue LowerFormalArguments(SDValue Chain, CallingConv::ID CallConv,
+                               bool isVarArg,
+                               const SmallVectorImpl<ISD::InputArg> &Ins,
+                               const SDLoc &dl, SelectionDAG &DAG,
+                               SmallVectorImpl<SDValue> &InVals) const override;
 
-//   SDValue LowerCall(TargetLowering::CallLoweringInfo &CLI,
-//                     SmallVectorImpl<SDValue> &InVals) const override;
+  SDValue LowerCall(TargetLowering::CallLoweringInfo &CLI,
+                    SmallVectorImpl<SDValue> &InVals) const override;
 
-//   SDValue LowerReturn(SDValue Chain, CallingConv::ID CallConv, bool isVarArg,
-//                       const SmallVectorImpl<ISD::OutputArg> &Outs,
-//                       const SmallVectorImpl<SDValue> &OutVals, const SDLoc &dl,
-//                       SelectionDAG &DAG) const override;
+  SDValue LowerReturn(SDValue Chain, CallingConv::ID CallConv, bool isVarArg,
+                      const SmallVectorImpl<ISD::OutputArg> &Outs,
+                      const SmallVectorImpl<SDValue> &OutVals, const SDLoc &dl,
+                      SelectionDAG &DAG) const override;
 
-//   bool CanLowerReturn(CallingConv::ID CallConv, MachineFunction &MF,
-//                       bool isVarArg,
-//                       const SmallVectorImpl<ISD::OutputArg> &ArgsFlags,
-//                       LLVMContext &Context) const override;
+  bool CanLowerReturn(CallingConv::ID CallConv, MachineFunction &MF,
+                      bool isVarArg,
+                      const SmallVectorImpl<ISD::OutputArg> &ArgsFlags,
+                      LLVMContext &Context) const override;
 
-//   bool mayBeEmittedAsTailCall(const CallInst *CI) const override;
-// };
+  bool mayBeEmittedAsTailCall(const CallInst *CI) const override;
+};
 
-// } // end namespace llvm
+} // end namespace llvm
 
 #endif // LLVM_LIB_TARGET_MBLAZE_MBLAZEISELLOWERING_H
