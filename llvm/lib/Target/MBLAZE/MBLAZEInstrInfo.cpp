222227212a9a6ff910a2a235a7ac1b210ba29431
MaxBubblegum47 <lorenzostigliano@gmail.com>
2022-10-03 18:20:33 +0200

    Adding more stuff: anchor and fixing intrinfo

--- llvm/lib/Target/MBLAZE/MBLAZEInstrInfo.cpp
+++ llvm/lib/Target/MBLAZE/MBLAZEInstrInfo.cpp
@@ -1,16 +1,12 @@
-#include "MBLAZE.h"
-#include "MBLAZERegisterInfo.h"
 #include "MBLAZEInstrInfo.h"
-#include "llvm/ADT/STLExtras.h"
-#include "llvm/ADT/SmallVector.h"
-#include "llvm/Analysis/MemoryLocation.h"
-#include "llvm/CodeGen/LiveVariables.h"
-#include "llvm/CodeGen/MachineFunctionPass.h"
+#include "MBLAZE.h"
+#include "MBLAZEMachineFunctionInfo.h"
+#include "MBLAZESubtarget.h"
+#include "MCTargetDesc/MBLAZEInfo.h"
+#include "llvm/CodeGen/MachineFrameInfo.h"
 #include "llvm/CodeGen/MachineInstrBuilder.h"
-#include "llvm/CodeGen/MachineRegisterInfo.h"
-#include "llvm/CodeGen/RegisterScavenging.h"
-#include "llvm/MC/MCInstBuilder.h"
-#include "llvm/Support/ErrorHandling.h"
+#include "llvm/CodeGen/MachineMemOperand.h"
+#include "llvm/Support/Debug.h"
 #include "llvm/Support/TargetRegistry.h"
 
 using namespace llvm;
@@ -18,7 +14,10 @@ using namespace llvm;
 #define GET_INSTRINFO_CTOR_DTOR
 #include "MBLAZEGenInstrInfo.inc"
 
+
+void MBLAZEInstrInfo::anchor() {}
+
 MBLAZEInstrInfo::MBLAZEInstrInfo()
-    : MBLAZEGenInstrInfo(MBLAZE::ADJCALLSTACKDOWN, MBLAZE::ADJCALLSTACKUP) {}
+    : MBLAZEGenInstrInfo(MBLAZE::ADJCALLSTACKDOWN, MBLAZE::ADJCALLSTACKUP), RI() {}
 
 
