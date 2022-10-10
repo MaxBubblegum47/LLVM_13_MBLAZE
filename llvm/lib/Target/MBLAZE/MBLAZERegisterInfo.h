aebe26545efab5588356643ad53dcedbd2336666
MaxBubblegum47 <lorenzostigliano@gmail.com>
2022-10-10 12:37:25 +0200

    Better formatting the file

--- llvm/lib/Target/MBLAZE/MBLAZERegisterInfo.h
+++ llvm/lib/Target/MBLAZE/MBLAZERegisterInfo.h
@@ -27,37 +27,18 @@ public:
   MBLAZERegisterInfo();
 
   /// Code Generation virtual methods...
-
-  //done
   const MCPhysReg *getCalleeSavedRegs(const MachineFunction *MF) const override;
-
-  // done
   BitVector getReservedRegs(const MachineFunction &MF) const override;
-
-  // done
   bool requiresRegisterScavenging(const MachineFunction &MF) const override;
-
-  // done
   bool useFPForScavengingIndex(const MachineFunction &MF) const override;
-
-  // done
   void eliminateFrameIndex(MachineBasicBlock::iterator II, int SPAdj,
                            unsigned FIOperandNum,
                            RegScavenger *RS = nullptr) const override;
-
-  // done
   const uint32_t *getCallPreservedMask(const MachineFunction &MF,
                                        CallingConv::ID CC) const override;
-
-  // Debug information queries. done
   Register getFrameRegister(const MachineFunction &MF) const override;
-
-  //! Return whether to emit frame moves
   static bool needsFrameMoves(const MachineFunction &MF);
-};
-
-
-
+  
+  };
 } // end namespace llvm
-
 #endif // LLVM_LIB_TARGET_MBLAZE_MBLAZEREGISTERINFO_H
