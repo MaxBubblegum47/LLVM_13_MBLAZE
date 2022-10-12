0d90367d18e93833cb6e41ce3c74a2df267fa6e7
MaxBubblegum47 <lorenzostigliano@gmail.com>
2022-10-12 21:57:11 +0200

    Adding new register lowering

--- llvm/lib/Target/MBLAZE/MBLAZERegisterInfo.h
+++ llvm/lib/Target/MBLAZE/MBLAZERegisterInfo.h
@@ -27,18 +27,29 @@ public:
   MBLAZERegisterInfo();
 
   /// Code Generation virtual methods...
+
   const MCPhysReg *getCalleeSavedRegs(const MachineFunction *MF) const override;
+
   BitVector getReservedRegs(const MachineFunction &MF) const override;
+
   bool requiresRegisterScavenging(const MachineFunction &MF) const override;
+
   bool useFPForScavengingIndex(const MachineFunction &MF) const override;
+
   void eliminateFrameIndex(MachineBasicBlock::iterator II, int SPAdj,
                            unsigned FIOperandNum,
                            RegScavenger *RS = nullptr) const override;
+
   const uint32_t *getCallPreservedMask(const MachineFunction &MF,
                                        CallingConv::ID CC) const override;
+
+  // Debug information queries.
   Register getFrameRegister(const MachineFunction &MF) const override;
+
+  //! Return whether to emit frame moves
   static bool needsFrameMoves(const MachineFunction &MF);
-  
-  };
+};
+
 } // end namespace llvm
+
 #endif // LLVM_LIB_TARGET_MBLAZE_MBLAZEREGISTERINFO_H
