d0530fb1fe3f04f7c06635db30d30a2c6ef9685d
MaxBubblegum47 <lorenzostigliano@gmail.com>
2022-10-21 16:43:15 +0200

    First commit

---
+++ llvm/lib/Target/MBLAZE/MBLAZETargetTransformInfo.h
@@ -0,0 +1,54 @@
+//===- MBLAZETargetTransformInfo.h - MBLAZE specific TTI --------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+// \file
+// This file contains a TargetTransformInfo::Concept conforming object specific
+// to the MBLAZE target machine. It uses the target's detailed information to
+// provide more precise answers to certain TTI queries, while letting the
+// target independent and default TTI implementations handle the rest.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIB_TARGET_MBLAZE_MBLAZETARGETTRANSFORMINFO_H
+#define LLVM_LIB_TARGET_MBLAZE_MBLAZETARGETTRANSFORMINFO_H
+
+#include "MBLAZE.h"
+#include "llvm/Analysis/TargetTransformInfo.h"
+#include "llvm/CodeGen/BasicTTIImpl.h"
+
+namespace llvm {
+
+class MBLAZESubtarget;
+class MBLAZETargetLowering;
+class MBLAZETargetMachine;
+
+class MBLAZETTIImpl : public BasicTTIImplBase<MBLAZETTIImpl> {
+  using BaseT = BasicTTIImplBase<MBLAZETTIImpl>;
+  friend BaseT;
+
+  const MBLAZESubtarget *ST;
+  const MBLAZETargetLowering *TLI;
+
+  const MBLAZESubtarget *getST() const { return ST; }
+  const MBLAZETargetLowering *getTLI() const { return TLI; }
+
+public:
+  explicit MBLAZETTIImpl(const MBLAZETargetMachine *TM, const Function &F)
+      : BaseT(TM, F.getParent()->getDataLayout()), ST(TM->getSubtargetImpl()),
+        TLI(ST->getTargetLowering()) {}
+
+  // Provide value semantics. MSVC requires that we spell all of these out.
+  MBLAZETTIImpl(const MBLAZETTIImpl &Arg)
+      : BaseT(static_cast<const BaseT &>(Arg)), ST(Arg.ST), TLI(Arg.TLI) {}
+  MBLAZETTIImpl(MBLAZETTIImpl &&Arg)
+      : BaseT(std::move(static_cast<BaseT &>(Arg))), ST(std::move(Arg.ST)),
+        TLI(std::move(Arg.TLI)) {}
+};
+
+} // end namespace llvm
+
+#endif // LLVM_LIB_TARGET_MBLAZE_MBLAZETARGETTRANSFORMINFO_H
