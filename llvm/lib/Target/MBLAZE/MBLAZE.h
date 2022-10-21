1ba325b0140a39c2e1b2016a368e65f996e7c677
MaxBubblegum47 <lorenzostigliano@gmail.com>
2022-10-21 16:39:50 +0200

    Improve Formatting

--- llvm/lib/Target/MBLAZE/MBLAZE.h
+++ llvm/lib/Target/MBLAZE/MBLAZE.h
@@ -1,34 +1,33 @@
-//===-- MBLAZE.h - Top-level interface for MBLAZE representation ----*- C++ -*-===//
+//===- MBLAZE.h - Top-level interface for MBLAZE representation -------*- C++ -*-===//
 //
-//
-//                      The LLVM Compiler Infrastructure
-//
-//
-//
-//===----------------------------------------------------------------------===//
-//
-// This file contains the entry points for global functions defined in
-// the LLVM MBLAZE back-end.
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
+//
+// This file contains the entry points for global functions defined in the LLVM
+// MBLAZE back-end.
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef LLVM_LIB_TARGET_MBLAZE_MBLAZE_H
 #define LLVM_LIB_TARGET_MBLAZE_MBLAZE_H
 
-//#include "MBLAZEConfig.h"
 #include "MCTargetDesc/MBLAZEMCTargetDesc.h"
 #include "llvm/Target/TargetMachine.h"
 
 namespace llvm {
-    
-    class FunctionPass;
-    class MBLAZETargetMachine;
 
-    FunctionPass *createMBLAZEISelDag(MBLAZETargetMachine &TM,
+class FunctionPass;
+class MBLAZETargetMachine;
+
+FunctionPass *createMBLAZEISelDag(MBLAZETargetMachine &TM,
                                CodeGenOpt::Level OptLevel);
+FunctionPass *createMBLAZEExpandPseudosPass();
+FunctionPass *createMBLAZEOptAddrMode();
+FunctionPass *createMBLAZEBranchFinalizePass();
 
-    FunctionPass *createMBLAZEExpandPseudosPass();
-    FunctionPass *createMBLAZEOptAddrMode();
-    FunctionPass *createMBLAZEBranchFinalizePass();
+} // end namespace llvm
 
-} // end namespace llvm;
-#endif
+#endif // LLVM_LIB_TARGET_MBLAZE_MBLAZE_H
