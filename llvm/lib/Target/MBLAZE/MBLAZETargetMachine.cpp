35084c2c81c7121dfd2716f58c0baf75d0f7fae2
MaxBubblegum47 <lorenzostigliano@gmail.com>
2022-10-21 16:42:58 +0200

    Adding some info taken from other backend

--- llvm/lib/Target/MBLAZE/MBLAZETargetMachine.cpp
+++ llvm/lib/Target/MBLAZE/MBLAZETargetMachine.cpp
@@ -1,36 +1,40 @@
-#include "MBLAZE.h"
+//===- MBLAZETargetMachine.cpp - Define TargetMachine for MBLAZE ------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+//
+//===----------------------------------------------------------------------===//
+
 #include "MBLAZETargetMachine.h"
+#include "MBLAZE.h"
+#include "MBLAZETargetTransformInfo.h"
 #include "TargetInfo/MBLAZETargetInfo.h"
+#include "llvm/CodeGen/Passes.h"
 #include "llvm/CodeGen/TargetLoweringObjectFileImpl.h"
 #include "llvm/CodeGen/TargetPassConfig.h"
 #include "llvm/Support/TargetRegistry.h"
 
 using namespace llvm;
 
-extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeMBLAZETarget() {
-  RegisterTargetMachine<MBLAZETargetMachine> X(getTheMBLAZETarget());
+static Reloc::Model getRelocModel(Optional<Reloc::Model> RM) {
+  return RM.getValueOr(Reloc::Static);
 }
 
-static std::string computeDataLayout(const Triple &TT) {
-  std::string Ret;
-
-  // Only support little endian for now.
-  Ret += "e";
-
-  // This is just a sample
-  Ret += "-m:e-S32-p:32:32";
-
-  return Ret;
-}
-
+/// MBLAZETargetMachine ctor - Create an ILP32 MBLAZEhitecture model
 MBLAZETargetMachine::MBLAZETargetMachine(const Target &T, const Triple &TT,
-                                     StringRef CPU, StringRef FS,
-                                     const TargetOptions &Options,
-                                     Optional<Reloc::Model> RM,
-                                     Optional<CodeModel::Model> CM,
-                                     CodeGenOpt::Level OL, bool JIT)
-    : LLVMTargetMachine(T, computeDataLayout(TT), TT, CPU, FS, Options,
-                        RM.getValueOr(Reloc::Static),
+                                   StringRef CPU, StringRef FS,
+                                   const TargetOptions &Options,
+                                   Optional<Reloc::Model> RM,
+                                   Optional<CodeModel::Model> CM,
+                                   CodeGenOpt::Level OL, bool JIT)
+    : LLVMTargetMachine(T,
+                        "e-m:e-p:32:32-i1:8:32-i8:8:32-i16:16:32-i32:32:32-"
+                        "f32:32:32-i64:32-f64:32-a:0:32-n32",
+                        TT, CPU, FS, Options, getRelocModel(RM),
                         getEffectiveCodeModel(CM, CodeModel::Small), OL),
       TLOF(std::make_unique<TargetLoweringObjectFileELF>()),
       Subtarget(TT, std::string(CPU), std::string(FS), *this) {
@@ -40,26 +44,24 @@ MBLAZETargetMachine::MBLAZETargetMachine(const Target &T, const Triple &TT,
 MBLAZETargetMachine::~MBLAZETargetMachine() = default;
 
 namespace {
+
+/// MBLAZE Code Generator Pass Configuration Options.
 class MBLAZEPassConfig : public TargetPassConfig {
 public:
   MBLAZEPassConfig(MBLAZETargetMachine &TM, PassManagerBase &PM)
-      : TargetPassConfig(TM, PM) {
-      
-      }
+      : TargetPassConfig(TM, PM) {}
 
   MBLAZETargetMachine &getMBLAZETargetMachine() const {
     return getTM<MBLAZETargetMachine>();
   }
 
   bool addInstSelector() override;
-  // void addPreEmitPass() override;
-  // void addPreRegAlloc() override;
+  void addPreEmitPass() override;
+  void addPreRegAlloc() override;
 };
 
-} // namespace
+} // end anonymous namespace
 
-
-//NEW CONFIGURATION TAKEN FROM MBLAZE
 TargetPassConfig *MBLAZETargetMachine::createPassConfig(PassManagerBase &PM) {
   return new MBLAZEPassConfig(*this, PM);
 }
@@ -69,33 +71,19 @@ bool MBLAZEPassConfig::addInstSelector() {
   return false;
 }
 
-//void MBLAZEPassConfig::addPreEmitPass() { addPass(createMBLAZEBranchFinalizePass()); }
+void MBLAZEPassConfig::addPreEmitPass() { addPass(createMBLAZEBranchFinalizePass()); }
 
-// void MBLAZEPassConfig::addPreRegAlloc() {
-//     addPass(createMBLAZEExpandPseudosPass());
-//     addPass(createMBLAZEOptAddrMode());
-// }
-
-
-
-// OLD CONFIGURATION
-// TargetPassConfig *MBLAZETargetMachine::createPassConfig(PassManagerBase &PM) {
-//   //  addISelPrepare();
-    
-//   return new MBLAZEPassConfig(*this, PM);
-// }
-
-// bool MBLAZEPassConfig::addInstSelector() {
-//   addPass(createMBLAZEISelDag(getMBLAZETargetMachine(), getOptLevel()));
-//   return false;
-// }
+void MBLAZEPassConfig::addPreRegAlloc() {
+    addPass(createMBLAZEExpandPseudosPass());
+    addPass(createMBLAZEOptAddrMode());
+}
 
 // Force static initialization.
-// extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeMBLAZETarget() {
-//   RegisterTargetMachine<MBLAZETargetMachine> X(getTheMBLAZETarget());
-// }
+extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeMBLAZETarget() {
+  RegisterTargetMachine<MBLAZETargetMachine> X(getTheMBLAZETarget());
+}
 
-// TargetTransformInfo
-// MBLAZETargetMachine::getTargetTransformInfo(const Function &F) {
-//   return TargetTransformInfo(MBLAZETTIImpl(this, F));
-// }
+TargetTransformInfo
+MBLAZETargetMachine::getTargetTransformInfo(const Function &F) {
+  return TargetTransformInfo(MBLAZETTIImpl(this, F));
+}
