3958b1ec04a117daf39e44266e53a005b3520df8
MaxBubblegum47 <lorenzostigliano@gmail.com>
2022-10-21 16:45:40 +0200

    Improve formatting and adding comments

--- llvm/lib/Target/MBLAZE/TargetInfo/MBLAZETargetInfo.cpp
+++ llvm/lib/Target/MBLAZE/TargetInfo/MBLAZETargetInfo.cpp
@@ -1,19 +1,15 @@
-//===-- MBLAZETargetInfo.cpp - MBLAZE Target Implementation -------------------===//
+//===- MBLAZETargetInfo.cpp - MBLAZE Target Implementation ----------- *- C++ -*-===//
 //
-//
-//                  The LLVM Compiler Infrastructure
-//
-// This file is distributed under the University of Illinois Open Source
-// License. See LICENSE.TXT for details.
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
+
 #include "TargetInfo/MBLAZETargetInfo.h"
 #include "llvm/Support/TargetRegistry.h"
 
 using namespace llvm;
-
-
-//Target llvm::TheMBLAZETarget, llvm::TheMBLAZEelTarget;
 
 Target &llvm::getTheMBLAZETarget() {
   static Target TheMBLAZETarget;
--- llvm/lib/Target/MBLAZE/TargetInfo/MBLAZETargetInfo.h
+++ llvm/lib/Target/MBLAZE/TargetInfo/MBLAZETargetInfo.h
@@ -1,3 +1,11 @@
+//===- MBLAZETargetInfo.h - MBLAZE Target Implementation ------------- *- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef LLVM_LIB_TARGET_MBLAZE_TARGETINFO_MBLAZETARGETINFO_H
 #define LLVM_LIB_TARGET_MBLAZE_TARGETINFO_MBLAZETARGETINFO_H
 
